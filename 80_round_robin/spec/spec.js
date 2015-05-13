0756fcd45e2610cd5922148dfd7951ce1ae913bf
Andreas Kavountzis <akavount@gmail.com>
2015-05-12 22:20:56 -0600

    Corrected spec

--- 80_round_robin/spec/spec.js
+++ 80_round_robin/spec/spec.js
@@ -1,35 +1,34 @@
 var roundRobin = require('../src');
 
 describe('roundRobin', function () {
-
   it('returns rounds for 6 correctly', function () {
     pending();
     var input = [1,2,3,4,5,6];
     var result = roundRobin(input);
     expect(result[0]).toEqual([
-      [1,4],
+      [1,6],
       [2,5],
-      [3,6],
+      [3,4],
     ]);
     expect(result[1]).toEqual([
       [1,5],
-      [4,6],
+      [6,4],
       [2,3],
     ]);
     expect(result[2]).toEqual([
-      [1,6],
+      [1,4],
       [5,3],
-      [4,2],
+      [6,2],
     ]);
     expect(result[3]).toEqual([
       [1,3],
-      [6,2],
-      [5,4]
+      [4,2],
+      [5,6]
     ]);
     expect(result[4]).toEqual([
       [1,2],
-      [3,4],
-      [6,5],
+      [3,6],
+      [4,5],
     ]);
     expect(result[5]).toEqual(undefined);
   });
@@ -39,48 +38,47 @@ describe('roundRobin', function () {
     var input = [1,2,3,4,5,6,7,8];
     var result = roundRobin(input);
     expect(result[0]).toEqual([
-      [1,5],
-      [2,6],
-      [3,7],
-      [4,8],
+      [1,8],
+      [2,7],
+      [3,6],
+      [4,5],
     ]);
     expect(result[1]).toEqual([
-      [1,6],
-      [5,7],
-      [2,8],
+      [1,7],
+      [8,6],
+      [2,5],
       [3,4],
     ]);
     expect(result[2]).toEqual([
-      [1,7],
-      [6,8],
-      [5,4],
+      [1,6],
+      [7,5],
+      [8,4],
       [2,3],
     ]);
     expect(result[3]).toEqual([
-      [1,8],
-      [7,4],
-      [6,3],
-      [5,2],
+      [1,5],
+      [6,4],
+      [7,3],
+      [8,2],
     ]);
     expect(result[4]).toEqual([
       [1,4],
-      [8,3],
-      [7,2],
-      [6,5],
+      [5,3],
+      [6,2],
+      [7,8],
     ]);
     expect(result[5]).toEqual([
       [1,3],
       [4,2],
-      [8,5],
-      [7,6],
+      [5,8],
+      [6,7],
     ]);
     expect(result[6]).toEqual([
       [1,2],
-      [3,5],
-      [4,6],
-      [8,7],
+      [3,8],
+      [4,7],
+      [5,6],
     ]);
     expect(result[7]).toEqual(undefined);
   });
-
 });
