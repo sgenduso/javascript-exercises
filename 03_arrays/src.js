4a0db2498c929e8562f45ff0d97d8964d3bdecd3
Sam Genduso <sam.genduso@gmail.com>
2015-12-13 21:06:56 -0700

    03 first 4 functions tests passing

--- 03_arrays/src.js
+++ 03_arrays/src.js
@@ -1,7 +1,27 @@
 module.exports = {
 
   first: function(array) {
-    // fill in code here
+    return array[0];
+  },
+
+  last: function (array) {
+    return array[array.length-1];
+  },
+
+  empty: function (array) {
+    return !array.length;
+  },
+
+  first_n: function (array, n) {
+    if (array.length === 0 || array.length < n) {
+      return array;
+    } else {
+      return array.splice(0, n);
+    }
+  },
+
+  last_n: function (array, n) {
+    
   }
 
-}
+};
