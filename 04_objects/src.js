dde31075e1eefc7d1a97561972955e443c5d71da
Sam Genduso <sam.genduso@gmail.com>
2015-12-15 22:11:38 -0700

    Update src.js

--- 04_objects/src.js
+++ 04_objects/src.js
@@ -1,5 +1,25 @@
 module.exports = {
   person: function() {
     // your code here
+  },
+  
+  sweetnessProperty: function() {
+    
+  },
+  
+  savorynessProperty: function() {
+    
+  },
+  
+  keys: function() {
+    
+  },
+  
+  sortedKeys: function() {
+    
+  },
+  
+  reverseSortedKeys: function() {
+    
   }
 }
