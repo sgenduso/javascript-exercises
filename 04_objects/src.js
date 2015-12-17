2f1454d98492722a2e6af5b258ca1d3ab6cb26f9
Sam Genduso <sam.genduso@gmail.com>
2015-12-16 21:36:44 -0700

    04 keys fn refactored

--- 04_objects/src.js
+++ 04_objects/src.js
@@ -15,11 +15,7 @@ module.exports = {
   },
 
   keys: function(obj) {
-    var keys = [];
-    for (var key in obj) {
-      keys.push(key);
-    }
-    return keys;
+    return Object.keys(obj);
   },
 
   sortedKeys: function() {
