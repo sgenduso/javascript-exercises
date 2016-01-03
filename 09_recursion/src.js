f1f5113ccd2394c43c3b0d9eebaf6bc79d5dabe7
Sam Genduso <sam.genduso@gmail.com>
2016-01-03 10:48:14 -0500

    09 factorial fn

--- 09_recursion/src.js
+++ 09_recursion/src.js
@@ -33,8 +33,15 @@ module.exports = {
   */
 
   factorial: function(n) {
-
-
-
+   if (n < 0) {
+       return -1;
+   }
+   else if (n == 0) {
+       return 1;
+   }
+   else {
+     return (n * factorial(n - 1))
+   }
   }
+  
 }
