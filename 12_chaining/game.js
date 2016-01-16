bd46fb7e70ab23a249ee441cfa47c247ba386523
Sam Genduso <sam.genduso@gmail.com>
2016-01-16 14:51:02 -0700

    game constructor

--- 12_chaining/game.js
+++ 12_chaining/game.js
@@ -1,3 +1,13 @@
 module.exports = {
+  function Game() {
+    this.score = 0;
+  }
 
+  Game.prototype.hit = function () {
+    return this.score + 1;
+  }
+
+  Game.prototype.miss = function () {
+    return this.score - 1;
+  }
 }
