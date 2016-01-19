0af92ced855cb164bff49cd862a9f8587608b7c8
Sam Genduso <sam.genduso@gmail.com>
2016-01-18 21:03:13 -0700

    reorg

--- 12_chaining/game.js
+++ 12_chaining/game.js
@@ -1,13 +1,19 @@
-module.exports = {
-  function Game() {
-    this.score = 0;
-  }
+    var Game = function() {
+      this.score = 0;
+    };
 
-  Game.prototype.hit = function () {
-    return this.score + 1;
-  }
+    Game.prototype.hit = function () {
+      console.log('hit');
+      this.score += 1;
+      return Game;
+    };
 
-  Game.prototype.miss = function () {
-    return this.score - 1;
-  }
-}
+    Game.prototype.miss = function () {
+      console.log('miss');
+      this.score -= 1;
+      return Game;
+    };
+
+    module.exports = {
+      Game: new Game(),
+    }
